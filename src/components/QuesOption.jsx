83cbcc9188679b5382b0c7f63375f3f6c1c20ed3
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-10-11 10:32:44 +0600

    added toastify answer

--- src/components/QuesOption.jsx
+++ src/components/QuesOption.jsx
@@ -13,7 +13,7 @@ const QuesOption = ({option, index, id, correctAnswer}) => {
     }
 
     return (
-        <div onClick={() =>getRightOrWrong(option)} className='text-lg'>
+        <div onClick={() =>getRightOrWrong(option)} className='text-lg border border-violet-300 pl-5 py-2' >
             <input type="radio" name={id} id={index} />
             <label htmlFor={index}>  {option}</label>
         </div>
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import QuesOption from './QuesOption';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { toast } from 'react-toastify';
 
 const Questions = ({questions, questionNumber}) => {
     const {options, question, id, correctAnswer} = questions;
+
+    const getCorrectAns = (ans) => {
+        toast.info(`Correct Answer: ${ans}`, {autoClose: 1000, theme: "colored"})
+    }
+
     return (
-        <div className='container mx-auto w-3/4 border border-slate-100 shadow-lg rounded-lg text-violet-900 font-mono mb-5'>
+        <div className='container mx-auto w-3/4 border border-slate-100 shadow-lg rounded-lg text-violet-900 font-mono mb-5 relative'>
+
             <h1 className='text-2xl text-center w-2/3 mx-auto mb-3'>Quiz {questionNumber}:  {question}</h1>
+
+            <div onClick={() => getCorrectAns(correctAnswer)} className='absolute top-3 right-5'>
+                <FontAwesomeIcon icon={faEye} />
+            </div>
+
             <div className='grid md:grid-cols-2 grid-cols-1 gap-5 px-5 pb-3'>
                 {
                     options.map((option, index) => <QuesOption
