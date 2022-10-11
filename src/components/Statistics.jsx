2febb0177a4859752d2d5c74cf49fe5d261ede3d
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-10-11 12:11:28 +0600

    added statistics

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Bar, BarChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
+import { QuizTopicsContext } from './Root';
 
 const Statistics = () => {
+    const quizTopics = useContext(QuizTopicsContext);
     return (
-        <div>
-            <h1>statistics</h1>
+        <div className='container mx-auto'>
+            <h1 className='text-center text-3xl font-bold text-purple-500 my-10'>Find Out How Many Questions Are There In Each Topic :</h1>
+            <div className='flex justify-center'>
+                <BarChart width={600} height={500} data={quizTopics}>
+                    <XAxis dataKey="name" stroke="#fa7b0c" />
+                    <YAxis />
+                    <Tooltip />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <Bar dataKey="total" barSize={50} fill="#8884d8" />
+                </BarChart>
+            </div>
         </div>
     );
 };
