4124e9c57d9c95839b5546c9e2f49f617914b3f9
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-10-12 10:29:22 +0600

    chart responsive

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Bar, BarChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import { QuizTopicsContext } from './Root';
 
 const Statistics = () => {
@@ -8,13 +8,15 @@ const Statistics = () => {
         <div className='container mx-auto'>
             <h1 className='text-center text-3xl font-bold text-purple-500 my-10'>Find Out How Many Questions Are There In Each Topic :</h1>
             <div className='flex justify-center'>
-                <BarChart width={600} height={500} data={quizTopics}>
-                    <XAxis dataKey="name" stroke="#fa7b0c" />
-                    <YAxis />
-                    <Tooltip />
-                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                    <Bar dataKey="total" barSize={50} fill="#8884d8" />
-                </BarChart>
+                <ResponsiveContainer width="75%" height={450}>
+                    <BarChart data={quizTopics}>
+                        <XAxis dataKey="name" stroke="#fa7b0c" />
+                        <YAxis />
+                        <Tooltip />
+                        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                        <Bar dataKey="total" barSize={40} fill="#8884d8" />
+                    </BarChart>
+                </ResponsiveContainer>
             </div>
         </div>
     );
